c14435a9f72181e207234c0e6e0bcd32e53acdf4
Parthajeet-Sarmah <coderparth32768@gmail.com>
2024-05-30 02:03:04 +0530

    Update NNFS_GD

--- nn.py
+++ nn.py
@@ -4,7 +4,7 @@ import numpy as np
 import pickle
 import gensim.downloader as api
 
-with open("model.pkl", "rb") as f:
+with open("model_two.pkl", "rb") as f:
     model_data = pickle.load(f)
 
 weights1 = model_data["weights1"]
@@ -15,23 +15,27 @@ bias2 = model_data["bias2"]
 # Load your word embedding model
 model = api.load("word2vec-google-news-300")
 
-
 def softmax(x):
     exp_x = np.exp(x - np.max(x))
     return exp_x / exp_x.sum(axis=0)
 
-# Prediction function using the loaded model
 def predicted_output(emb):
-    l = [max(0, emb[0]*weights1[0]+emb[1]*weights1[1]+emb[2]*weights1[2]+emb[3]*weights1[3]+emb[4]*weights1[4]+ bias1), 
-         max(0, emb[0]*weights2[0]+emb[1]*weights2[1]+emb[2]*weights2[2]+emb[3]*weights2[3]+emb[4]*weights2[4]+ bias2)]
-    
-    prediction = softmax(l)
-    return prediction
+    predictedt = []
+    l1 = [
+            np.max([0, np.dot(j, hweights1) + hbias1]),
+            np.max([0, np.dot(j, hweights2) + hbias2]),
+            np.max([0, np.dot(j, hweights3) + hbias3])
+        ]
+    l2 = [
+        np.dot(l1, weights1) + bias1,
+        np.dot(l1, weights2) + bias2
+    ]
+    return softmax(l2)
 
 # Function to predict if a word is positive or negative
 def predict(word):
     if word in model.key_to_index:
-        emb = model[word][:5]  # Extract the first 5 dimensions of the word embedding
+        emb = model[word][:100]  # Extract the first 5 dimensions of the word embedding
     else:
         return None
 
